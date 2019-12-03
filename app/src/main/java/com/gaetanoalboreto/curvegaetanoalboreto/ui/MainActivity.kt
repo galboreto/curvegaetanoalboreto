692beea315bab1df2be288c325629651e870fa16
Gaetano Alboreto <>
2019-12-03 14:59:18 +0000

    Add progressbar for loading

--- app/src/main/java/com/gaetanoalboreto/curvegaetanoalboreto/ui/MainActivity.kt
+++ app/src/main/java/com/gaetanoalboreto/curvegaetanoalboreto/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gaetanoalboreto.curvegaetanoalboreto.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.SimpleItemAnimator
 import com.gaetanoalboreto.curvegaetanoalboreto.R
@@ -22,9 +23,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         (recyclerView.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
 
-
         viewModel.getPopularMovies().observe(this, Observer {
             adapter.submitList(it)
+            if (it.loadedCount != 0) loadingProgressBar.visibility = GONE
         })
     }
 }
